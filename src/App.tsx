a985f34b4ae4956fdd6cf8b144cfcfc221034726
Toni Mitrevski <toni@stativa.com.mk>
2020-06-11 12:38:22 +0200

    remove unuse useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.scss';
 import Header from "./Components/Header/Header";
 import MainContainer from "./Components/MainContainer/MainContainer";
