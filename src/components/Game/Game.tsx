6dc1081aa1686cafe3724b939b467f9319d26f59
Toni Mitrevski <toni@stativa.com.mk>
2020-06-15 20:52:08 +0200

    add jackpot imlementation to game and fix css, html issues

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -15,10 +15,7 @@ const Game = (props: { game: GameInterface, jackpots: JackpotStateInterface }) =
       <div className="Game">
           <LazyLoad once={true} offset={100} height={150} debounce={500} placeholder={<Placeholder/>}>
             <div className="Game__container">
-              <div className={`Game__container__ribbon`}>
-                <span className="Game__container__ribbon__label">top
-                </span>
-              </div>
+              {showTopRibbon(game)}
               <img src={game.image} alt={game.name}/>
               {showJackpot(jackpots.data, game)}
             </div>
@@ -27,11 +24,36 @@ const Game = (props: { game: GameInterface, jackpots: JackpotStateInterface }) =
   );
 }
 
+function showTopRibbon(game: GameInterface) {
+  if(!game.categories.includes("new")) {
+    return '';
+  }
+
+  return (
+    <div className={`Game__container__ribbon`}>
+      <span className="Game__container__ribbon__label">new
+      </span>
+    </div>
+  );
+}
+
 function showJackpot(jackpotAggregate: JackpotsAggregate, game: GameInterface) {
   if(!jackpotAggregate.hasOwnProperty(game.id)) {
     return '';
   }
-  return <span className="Game__container__jackpot">{jackpotAggregate[game.id].amount}</span>
+  return (
+    <div className="Game__container__jackpot">
+      <span>{currencyFormat(jackpotAggregate[game.id].amount)}</span>
+    </div>
+  );
+}
+
+function currencyFormat(amount: number) {
+  return new Intl.NumberFormat('en-UK', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 2
+  }).format(amount)
 }
 
 const mapStateToProps = (state: StoreType) => ({
