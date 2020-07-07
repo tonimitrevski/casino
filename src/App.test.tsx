2397eb6b08b5dff5066f474e2560ab6c0e1b6626
Toni Mitrevski <toni@stativa.com.mk>
2020-07-07 16:35:44 +0200

    write test for App component

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import App from "./App";
+import {shallow} from "enzyme";
+import React from 'react';
+import Header from "./components/Header/Header";
+import SiteRouter from "./core/SiteRouter/SiteRouter";
+import { BrowserRouter as Router } from 'react-router-dom'
+
+describe("App Component", () => {
+  test('check the component what are used on App component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find(".App").length).toBe(1);
+    expect(wrapper.find(Header).length).toBe(1);
+    expect(wrapper.find(SiteRouter).length).toBe(1);
+    expect(wrapper.find(Router).length).toBe(1);
+  })
+})
