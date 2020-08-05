4becc7d0484a85e257d4278de907b4b6dd945fa5
tonimitrevski <toni@stativa.com.mk>
2020-08-05 09:03:25 +0000

    Deploying to gh-pages from  @ 952bef6401ed650d3421e276e5c92ee27de11463 🚀

---
+++ precache-manifest.16a2e715e14e470a767b960affed2a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792a80e64106bf6ff4718723ff626d40",
+    "url": "/casino/index.html"
+  },
+  {
+    "revision": "92de53af79be90dc73d9",
+    "url": "/casino/static/css/main.b7a41651.chunk.css"
+  },
+  {
+    "revision": "02da340b910b649df199",
+    "url": "/casino/static/js/2.6cad4937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/casino/static/js/2.6cad4937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92de53af79be90dc73d9",
+    "url": "/casino/static/js/main.bfea817d.chunk.js"
+  },
+  {
+    "revision": "6178e4c7d6c420691879",
+    "url": "/casino/static/js/runtime-main.bc2fe8b3.js"
+  }
+]);
